66a4d39e9fc6e8b4f3ed10a4e9f4fbed7ebaee99
Lucas Mota <lucasmoota1@gmail.com>
2025-10-30 14:26:46 -0300

    fix(orm,core,schedule,queue): fix entry name

--- packages/schedule/src/entry.ts
+++ packages/schedule/src/entry.ts
@@ -1,6 +1,6 @@
 import { Cheetah } from '@cheetah.js/core';
 import { SchedulerOrchestration } from './scheduler-orchestration.service';
-import { SchedulerRegistry } from "@cheetah.js/schedule/scheduler.registry";
+import { SchedulerRegistry } from './scheduler.registry';
 
 export const CheetahScheduler = new Cheetah({
   exports: [SchedulerOrchestration, SchedulerRegistry]
