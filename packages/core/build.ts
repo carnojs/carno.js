a788ddd0b2ac604f0a105a87d16a82bb4d9e9f6f
Lucas Mota <lucasmoota1@gmail.com>
2026-01-08 21:47:01 -0300

    fix: build

---
+++ packages/core/build.ts
@@ -0,0 +1,46 @@
+import { $ } from 'bun'
+import { build, type Options } from 'tsup'
+import { fixImportsPlugin } from 'esbuild-fix-imports-plugin'
+
+import pack from './package.json'
+
+const external = [
+    ...Object.keys(pack.dependencies ?? {}),
+    ...Object.keys(pack.peerDependencies ?? {})
+]
+
+await $`rm -rf dist`
+
+await build({
+    entry: ['src/**/*.ts'],
+    outDir: 'dist',
+    format: ['esm', 'cjs'],
+    target: 'node20',
+    minifySyntax: true,
+    minifyWhitespace: false,
+    minifyIdentifiers: false,
+    splitting: false,
+    sourcemap: false,
+    cjsInterop: false,
+    clean: true,
+    bundle: false,
+    external,
+    esbuildPlugins: [fixImportsPlugin()]
+})
+
+await $`tsc --project tsconfig.json`
+
+await Bun.build({
+    entrypoints: ['./src/index.ts'],
+    outdir: './dist/bun',
+    minify: {
+        whitespace: true,
+        syntax: true,
+        identifiers: false
+    },
+    target: 'bun',
+    sourcemap: 'linked',
+    external
+})
+
+process.exit()
